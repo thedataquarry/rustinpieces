26b0d22e58b150a57993132fb110f74a122a8d06
prrao87 <prrao87@gmail.com>
2024-01-03 11:46:19 -0500

    Improve error handling and split into functions

--- rust/postgres_etl/src/bin/load_data.rs
+++ rust/postgres_etl/src/bin/load_data.rs
@@ -3,7 +3,7 @@ use serde::{Deserialize, Serialize};
 use sqlx::{postgres::PgPoolOptions, Pool, Postgres};
 use std::{error::Error, fs, path::Path};
 
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Serialize, Deserialize)]
 #[serde(rename_all = "camelCase")]
 struct Person {
     id: i32,
@@ -40,18 +40,12 @@ fn read_data(persons_csv_file: &Path) -> Result<Vec<Person>, csv::Error> {
     Ok(persons)
 }
 
-async fn get_connection() -> Pool<Postgres> {
-    let pg_uri = format!(
-        "postgres://postgres:{}@localhost:5432/etl",
-        dotenv::var("POSTGRES_PASSWORD").unwrap()
-    );
-    println!("{}", pg_uri);
+async fn get_connection_pool(pg_uri: &str) -> Result<Pool<Postgres>, sqlx::Error> {
     let pool: Pool<Postgres> = PgPoolOptions::new()
         .max_connections(5)
-        .connect(&pg_uri)
-        .await
-        .unwrap();
-    pool
+        .connect(pg_uri)
+        .await?;
+    Ok(pool)
 }
 
 async fn create_tables(
@@ -66,14 +60,38 @@ async fn create_tables(
     Ok(())
 }
 
-#[tokio::main]
-async fn main() {
+async fn insert_person(person: &Person, pool: &Pool<Postgres>) {
+    sqlx::query(
+        r#"
+            INSERT INTO persons (id, name, age, isMarried, city, state, country)
+            VALUES ($1, $2, $3, $4, $5, $6, $7)
+            "#,
+    )
+    .bind(person.id)
+    .bind(&person.name)
+    .bind(person.age)
+    .bind(person.is_married)
+    .bind(&person.city)
+    .bind(&person.state)
+    .bind(&person.country)
+    .execute(pool)
+    .await
+    .expect("Cannot insert data into persons table");
+}
+
+async fn run() -> Result<u32, Box<dyn Error>> {
     dotenv().ok();
     // Get files
     let persons_csv_file = Path::new("data/persons.csv");
     let persons_table_sql_file = Path::new("sql/create_persons_table.sql");
     // Create pool
-    let pool = get_connection().await;
+    let pg_uri = format!(
+        "postgres://postgres:{}@localhost:5432/etl",
+        dotenv::var("POSTGRES_PASSWORD").unwrap()
+    );
+    let pool = get_connection_pool(&pg_uri)
+        .await
+        .expect("Cannot create pool.");
 
     let persons: Vec<Person> =
         read_data(persons_csv_file).expect("Did not obtain valid output from CSV");
@@ -82,27 +100,18 @@ async fn main() {
 
     create_tables(persons_table_sql_file, &pool)
         .await
-        .expect("Cannot create table, please check SQL files.");
+        .expect("Cannot create table in database, please check SQL files.");
     // Populate database
     let mut counter: u32 = 0;
     for person in persons.iter() {
-        sqlx::query(
-            r#"
-                INSERT INTO persons (id, name, age, isMarried, city, state, country)
-                VALUES ($1, $2, $3, $4, $5, $6, $7)
-                "#,
-        )
-        .bind(person.id)
-        .bind(&person.name)
-        .bind(person.age)
-        .bind(person.is_married)
-        .bind(&person.city)
-        .bind(&person.state)
-        .bind(&person.country)
-        .execute(&pool)
-        .await
-        .expect("Cannot insert data into persons table");
+        insert_person(person, &pool).await;
         counter += 1;
     }
     println!("Finished loading {} records", counter);
+    Ok(counter)
+}
+
+#[tokio::main]
+async fn main() {
+    _ = run().await;
 }
