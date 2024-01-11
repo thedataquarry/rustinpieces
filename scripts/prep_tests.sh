a8b529226640b6986ebb4574a2745318feca3e2f
Paul Sanders <psanders1@gmail.com>
2024-01-11 11:22:59 -0500

    Sleep in action

--- scripts/prep_tests.sh
+++ scripts/prep_tests.sh
@@ -2,6 +2,3 @@
 
 cp pieces/postgres_etl/data/persons.csv scripts/data
 docker compose -f scripts/docker-compose.yml up -d
-wait 10
-docker ps
-docker compose -f scripts/docker-compose.yml logs database | tail
