be20d8c1888bdbd58331f261e67e825dd0897698
prrao87 <prrao87@users.noreply.github.com>
2024-01-18 18:07:43 +0000

    deploy: cd23ce20a8820aefbc98ec4abce2f0642e38ea7c

--- ci/prep_tests.sh
+++
@@ -1,5 +0,0 @@
-#!bin/bash
-
-mkdir ci/data
-cp pieces/postgres_etl/data/persons.csv ci/data
-docker compose -f ci/docker-compose.yml up -d
--- pieces/postgres_etl/sql/create_persons_table.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE IF NOT EXISTS persons(
-    id integer PRIMARY KEY,
-    name text,
-    age integer,
-    isMarried boolean,
-    city text,
-    state text,
-    country text
-)
