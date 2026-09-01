a26e327600a5d5ba6b00258ae8aa4b9ca5600598
prrao87 <prrao87@users.noreply.github.com>
2026-09-01 21:11:23 +0000

    deploy: 674429977d0b2ee5a3e98f7f38d97be8635b952a

--- searcher-09f2665d.js
+++ searcher-09f2665d.js
@@ -440,7 +440,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-d0760002.js',
+                path_to_root + 'searchindex-89d229d6.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
